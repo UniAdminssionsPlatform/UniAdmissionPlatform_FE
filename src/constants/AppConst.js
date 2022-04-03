a99a3e25540e3aaf533d3917101e8f3b686add23
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-04-04 00:42:42 +0700

    [FE] - Implement source source structure

---
+++ src/constants/AppConst.js
@@ -0,0 +1,3 @@
+export const TOKEN_KEY = 'token'
+export const TOKEN = ''
+export const USER_STORAGE = 'user'
---
+++ src/services/axiosBase.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import * as cookie from 'js-cookie'
+import {TOKEN_KEY} from '../constants/AppConst'
+
+export const CallAPI = (
+    endpoint,
+    method = 'GET',
+    body = {},
+    params = {},
+    configHeaders = null,
+    responseType = null
+) => {
+    let token = null
+    var headers = configHeaders
+        ? configHeaders
+        : {
+            'content-type': 'application/json'
+        }
+    token = cookie.get(TOKEN_KEY)
+    if (token && token !== 'undefined') {
+        headers.Authorization = `Bearer ${token}`
+    }
+
+    return axios({
+        method: method,
+        url: `${endpoint}`,
+        headers: headers,
+        data: body,
+        responseType: responseType,
+        params: params
+    })
+}
+
+export const CallAPIOutsite = (endpoint, method = 'GET', body) => {
+    var headers = {
+        'content-type': 'application/json'
+    }
+
+    return axios({
+        method: method,
+        url: `${endpoint}`,
+        headers: headers,
+        data: body
+    })
+}
