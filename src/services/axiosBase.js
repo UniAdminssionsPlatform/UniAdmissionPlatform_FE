24bcdd347c138abd9f0c7341a51f01dd087eb345
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-04-13 17:23:09 +0700

    [FE] - Implement passing CORS with middleware

--- src/services/axiosBase.js
+++ src/services/axiosBase.js
@@ -1,32 +1,32 @@
 import axios from 'axios'
 import * as cookie from 'js-cookie'
-import { TOKEN_KEY } from '../constants/AppConst'
+import {TOKEN_KEY} from '../constants/AppConst'
 
 export const CallAPI = (
-  endpoint,
-  method = 'GET',
-  body = {},
-  params = {},
-  configHeaders = null,
-  responseType = null
+    endpoint,
+    method = 'GET',
+    body = {},
+    params = {},
+    configHeaders = null,
+    responseType = null
 ) => {
-  let token = null
-  var headers = configHeaders
-    ? configHeaders
-    : {
-        'content-type': 'application/json'
-      }
-  token = cookie.get(TOKEN_KEY)
-  if (token && token !== 'undefined') {
-    headers.Authorization = `Bearer ${token}`
-  }
+    let token = null
+    var headers = configHeaders
+        ? configHeaders
+        : {
+            'content-type': 'application/json'
+        }
+    token = cookie.get(TOKEN_KEY)
+    if (token && token !== 'undefined') {
+        headers.Authorization = `Bearer ${token}`
+    }
 
-  return axios({
-    method: method,
-    url: `${endpoint}`,
-    headers: headers,
-    data: body,
-    responseType: responseType,
-    params: params
-  })
+    return axios({
+        method: method,
+        url: process.env.REACT_APP_API_URL + endpoint,
+        headers: headers,
+        data: body,
+        responseType: responseType,
+        params: params
+    })
 }
