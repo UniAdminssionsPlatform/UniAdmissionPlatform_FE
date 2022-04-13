4aa293b6fbeb67956e3bcab507031f7091b024e5
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-04-13 17:33:15 +0700

    [FE] - Implement passing CORS with middleware (#8)

--- src/services/axiosBase.js
+++ src/services/axiosBase.js
@@ -10,23 +10,23 @@ export const CallAPI = (
   configHeaders = null,
   responseType = null
 ) => {
-  let token = null
-  var headers = configHeaders
-    ? configHeaders
-    : {
-        'content-type': 'application/json'
-      }
-  token = cookie.get(TOKEN_KEY)
-  if (token && token !== 'undefined') {
-    headers.Authorization = `Bearer ${token}`
-  }
+    let token = null
+    var headers = configHeaders
+        ? configHeaders
+        : {
+            'content-type': 'application/json'
+        }
+    token = cookie.get(TOKEN_KEY)
+    if (token && token !== 'undefined') {
+        headers.Authorization = `Bearer ${token}`
+    }
 
-  return axios({
-    method: method,
-    url: `${endpoint}`,
-    headers: headers,
-    data: body,
-    responseType: responseType,
-    params: params
-  })
+    return axios({
+        method: method,
+        url: process.env.REACT_APP_API_URL + endpoint,
+        headers: headers,
+        data: body,
+        responseType: responseType,
+        params: params
+    })
 }
---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const { REACT_APP_API_URL } = process.env;
+module.exports = function (app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: `${REACT_APP_API_URL}`,
+            changeOrigin: true
+        })
+    );
+};
