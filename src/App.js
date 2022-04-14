ec0bc634d006f27bc402c5da691d72a5b95d78cd
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-04-15 03:18:50 +0700

    FIX (#12)
    * [FE] - intergrate template UI
    
    * [FE] - intergrate template UI
    
    * Disable Test
    
    * Config to CD
    
    * Config to CD

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import AppRouter from './router/AppRouter'
 import ErrorHandlerComponent from './components/commons/ErrorHandler/ErrorHandler.component'
-import React, { Suspense } from 'react'
+import React, {Suspense} from 'react'
 import store from './redux-flow/index'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <ErrorHandlerComponent>
-        <Provider store={store}>
-          <Suspense fallback="loading">
-            <div className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
-              <AppRouter />
-            </div>
-          </Suspense>
-        </Provider>
-      </ErrorHandlerComponent>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <ErrorHandlerComponent>
+                <Provider store={store}>
+                    <Suspense fallback="loading">
+                        <div className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
+                            <AppRouter/>
+                        </div>
+                    </Suspense>
+                </Provider>
+            </ErrorHandlerComponent>
+        </BrowserRouter>
+    )
 }
 
 export default App
