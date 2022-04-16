ce81a24a345d42e58dacada85df67a3cbed376eb
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-04-17 01:42:53 +0700

    [FIX] CI CD (#14)
    * [FE] - intergrate template UI
    
    * [FE] - intergrate template UI
    
    * Disable Test
    
    * Config to CD
    
    * Config to CD
    
    * Sovle conflig UI
    
    * mock data

---
+++ src/data/taxonomies.js
@@ -0,0 +1,13 @@
+import __taxonomies from './jsons/__taxonomies.json'
+
+const DEMO_CATEGORIES = __taxonomies.map(item => ({
+  ...item,
+  taxonomy: 'category'
+}))
+
+const DEMO_TAGS = __taxonomies.map(item => ({
+  ...item,
+  taxonomy: 'tag'
+}))
+
+export { DEMO_CATEGORIES, DEMO_TAGS }
