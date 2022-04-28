99c3417f96306ad56cdba61726c1313f48f61bb8
BanhsBao <huynhbaofaker@gmail.com>
2022-04-28 13:51:15 +0700

    [UAP-71] - Merge and resolve conflic

--- src/containers/authen/LoginContainer/Login.container.jsx
+++ src/containers/authen/LoginContainer/Login.container.jsx
@@ -7,7 +7,12 @@ import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { SigninHandler } from '../../../redux-flow/authentication/authentication-action';
 import { loginByFirebase } from '../../../services/UserServices';
-import { PATH } from '../../../constants/Paths/Path';
+import {
+    PATH,
+    PATH_HIGH_SCHOOL_MANAGER,
+    PATH_HIGH_SCHOOL_STUDENT,
+    PATH_UNIVERSITY_MANAGER
+} from '../../../constants/Paths/Path';
 import { HIGH_SCHOOL_MANAGER, HIGH_SCHOOL_STUDENT, UNIVERSITY_MANAGER } from '../../../constants/RoleType';
 import { handleNotification } from '../../../notification/LoginNotification';
 
@@ -20,14 +25,14 @@ const LoginContainer = () => {
 
   const handleRedirect = (user) => {
     if (user.need_register === true) {
-      history.push(PATH.REGISTER);
+      history.push(PATH.REGISTRATION);
     } else {
       if (user.roles === HIGH_SCHOOL_MANAGER) {
-        history.push(PATH.HIGH_SCHOOL_MANAGER.INDEX);
+        history.push(PATH_HIGH_SCHOOL_MANAGER.INDEX);
       } else if (user.roles === HIGH_SCHOOL_STUDENT) {
-        history.push(PATH.HIGH_SCHOOL_STUDENT.INDEX);
+        history.push(PATH_HIGH_SCHOOL_STUDENT.INDEX);
       } else if (user.roles === UNIVERSITY_MANAGER) {
-        history.push(PATH.UNIVERSITY_MANAGER.INDEX);
+        history.push(PATH_UNIVERSITY_MANAGER.INDEX);
       }
     }
   };
