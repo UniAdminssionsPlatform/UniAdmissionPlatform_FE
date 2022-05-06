cbbd9233fe35f0b8bea9830cf0c71f6e9020f44f
uikui <tinnt@gmail.com>
2022-05-07 00:12:53 +0700

    [sub-task] UAP-134: fix ui

--- src/components/majorGroup/components/PostFeaturedMedia/MajorMedia.component.jsx
+++ src/components/majorGroup/components/PostFeaturedMedia/MajorMedia.component.jsx
@@ -2,16 +2,13 @@ import React, { useRef } from 'react';
 import NcImage from '../../../commons/NcImage/NcImage.component';
 
 const MajorMedia = ({ className = ' w-full h-full ', post }) => {
-  const { featuredImage } = post;
+  const { thumbnailUrl } = post;
 
   const videoRef = useRef(null);
 
   return (
     <div className={`nc-PostFeaturedMedia relative ${className}`} data-nc-id='PostFeaturedMedia' ref={videoRef}>
-      <NcImage
-        containerClassName='absolute inset-0'
-        src='https://thietbiketnoi.com/wp-content/uploads/2020/01/tong-hop-hinh-nen-background-vector-designer-dep-do-phan-giai-fhd-2k-4k-moi-nhat-24-1024x678.jpg'
-      />
+      <NcImage containerClassName='absolute inset-0' src={thumbnailUrl} alt='Hình ảnh' />
     </div>
   );
 };
