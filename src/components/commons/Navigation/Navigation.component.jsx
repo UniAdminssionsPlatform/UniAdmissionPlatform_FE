49d5985b06bc7580a6f6bc3da041eedbb4820231
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-05-09 14:32:05 +0700

    [UAP-144] implement routing navigation

--- src/components/commons/Navigation/Navigation.component.jsx
+++ src/components/commons/Navigation/Navigation.component.jsx
@@ -7,7 +7,7 @@ const Navigation = () => {
   const { user } = useSelector((state) => state.authentication);
   return (
     <ul className='nc-Navigation hidden lg:flex lg:flex-wrap lg:items-center lg:space-x-1 relative'>
-      {NavigationSystem(user?.role).map((item) => (
+      {NavigationSystem(user?.roles).map((item) => (
         <NavigationItem key={item.id} menuItem={item} />
       ))}
     </ul>
--- src/router/navigation/component/UniversityManagerNavigation.js
+++ src/router/navigation/component/UniversityManagerNavigation.js
@@ -1,1 +1,65 @@
-export const UniversityManagerNavigation = [];
+import { PATH, PATH_UNIVERSITY_MANAGER } from '../../../constants/Paths/Path';
+import ncNanoId from '../../../utils/ncNanoId';
+
+const homePage = [
+  {
+    id: ncNanoId(),
+    name: 'Home Page',
+    href: PATH.INDEX
+  }
+];
+
+const eventManager = [
+  {
+    id: ncNanoId(),
+    href: PATH_UNIVERSITY_MANAGER.CREATE_EVENT,
+    name: 'Tạo sự kiện'
+  }
+];
+
+const eventMenu = [
+  {
+    id: ncNanoId(),
+    href: PATH_UNIVERSITY_MANAGER.NEW,
+    name: 'Danh sách news'
+  }
+];
+const profile = [
+  {
+    id: ncNanoId(),
+    href: PATH_UNIVERSITY_MANAGER.PROFILE,
+    name: 'Hồ sơ trường'
+  }
+];
+
+export const UniversityManagerNavigation = [
+  {
+    id: ncNanoId(),
+    href: '/',
+    name: 'Trang chủ',
+    type: '',
+    children: homePage
+  },
+  {
+    id: ncNanoId(),
+    href: '#',
+    name: 'Quản lý sự kiện',
+    type: 'dropdown',
+    children: eventManager
+  },
+
+  {
+    id: ncNanoId(),
+    href: '#',
+    name: 'Quản lý news',
+    type: 'dropdown',
+    children: eventMenu
+  },
+  {
+    id: ncNanoId(),
+    href: '#',
+    name: 'Quản lý hồ sơ',
+    type: 'dropdown',
+    children: profile
+  }
+];
