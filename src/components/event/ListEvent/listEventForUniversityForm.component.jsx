4a0117e378e7f292a206a1f0f1e731e52defc54a
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-05-17 01:29:00 +0700

    [UAP-116] Implement manage event created by role university (#47)
    * [UAP-109] - Implement get list event by Highschool
    
    * [UAP-112] implement Endpoint and service for create slot
    
    * [FE] - Implement get list event booked by university
    
    * [FE] - Implement get list event booked by university and search
    
    * [UAP-116] Implement manage event created by role university
    
    Co-authored-by: Nham Duc Dat <datndse141132@fpt.edu.vn>

---
+++ src/components/event/ListEvent/listEventForUniversityForm.component.jsx
@@ -0,0 +1,25 @@
+import LayoutPageWithout from '../../commons/LayoutPage/LayoutPageWithout.component';
+import ListEventForUniversity from './listEventForUniversity.component';
+import SearchBarComponent from './SearchBar.component';
+
+const ListEventForUniversityForm = (props) => {
+  const { eventforuniversity, setDataSearch } = props;
+  return (
+    <LayoutPageWithout
+      LayoutPage
+      subHeading='DANH SÁCH CÁC SỰ KIỆN TRƯỜNG ĐÃ TẠO'
+      headingEmoji='⚙'
+      heading='Dash board'>
+      <div className='flex flex-col space-y-20 xl:space-y-0 xl:flex-row'>
+        <div>
+          <SearchBarComponent setDataSearch={setDataSearch} />
+        </div>
+        <div className='shadow dark:border dark:border-neutral-800 overflow-hidden sm:rounded-lg ml-12'>
+          <ListEventForUniversity eventforuniversity={eventforuniversity} />
+        </div>
+      </div>
+    </LayoutPageWithout>
+  );
+};
+
+export default ListEventForUniversityForm;
---
+++ src/services/event/GetListEvent/GetListEventForUniversity.js
@@ -0,0 +1,9 @@
+import { PATH_GET_LIST_EVENT_FOR_UNIVERSITY } from '../../../constants/Endpoints/GetListEventForUniversity';
+import { CallAPI } from '../../axiosBase';
+
+export const EventForUniversity = (data) =>
+  CallAPI(
+    `${PATH_GET_LIST_EVENT_FOR_UNIVERSITY}/${data.universityID}/list-events?event-name=${data.name}&status-event=${data.status}&event-host-name=${data.hostname}&event-type-id=${data.eventtype}`,
+    'get',
+    data
+  );
