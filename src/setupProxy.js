fb27f608617dd3351e4b7fc4fa9a792e6bd08f05
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-05-22 01:21:35 +0700

    [HOT-FIX] Remo useless things (#58)
    * [UAP-166] - Fix Security domain layer/call api production
    
    * [UAP-166] - Fix Security domain layer/call api production
    
    * [UAP-166] - Fix Security domain layer/call api production
    
    * [UAP-166] - Fix Security domain layer/call api production

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-import * as helmet from 'helmet';
-
 const { REACT_APP_API_URL } = process.env;
-//setup meta to pass security
 module.exports = function (app) {
   app.use(
     '/api',
@@ -11,30 +8,4 @@ module.exports = function (app) {
       changeOrigin: true
     })
   );
-  app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'", 'data:', 'blob:'],
-        fontSrc: ["'self'", 'https:', 'data:'],
-        scriptSrcElem: ["'self'", 'https:', 'https://*.cloudflare.com'],
-        styleSrc: ["'self'", 'https:', 'unsafe-inline'],
-        connectSrc: ["'self'", 'data', 'https://*.cloudflare.com']
-      }
-    })
-  );
-  app.use(helmet.contentSecurityPolicy());
-  app.use(helmet.crossOriginEmbedderPolicy());
-  app.use(helmet.crossOriginOpenerPolicy());
-  app.use(helmet.crossOriginResourcePolicy());
-  app.use(helmet.dnsPrefetchControl());
-  app.use(helmet.expectCt());
-  app.use(helmet.frameguard());
-  app.use(helmet.hidePoweredBy());
-  app.use(helmet.hsts());
-  app.use(helmet.ieNoOpen());
-  app.use(helmet.noSniff());
-  app.use(helmet.originAgentCluster());
-  app.use(helmet.permittedCrossDomainPolicies());
-  app.use(helmet.referrerPolicy());
-  app.use(helmet.xssFilter());
 };
