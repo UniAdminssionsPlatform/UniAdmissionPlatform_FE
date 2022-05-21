a0bcb63762135968d905a254eeb5daaa403ebee4
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-05-21 22:38:32 +0700

    [UAP-167] - Migrate React version 18 to version 17 (#54)

--- src/pages/authen/ChangePasswordPage/ChangePasswordPage.jsx
+++ src/pages/authen/ChangePasswordPage/ChangePasswordPage.jsx
@@ -1,5 +1,6 @@
 import ChangePasswordContainer from '../../../components/authen/ChangePasswordComponent/ChangePasswordComponent';
 import React from 'react';
+
 const ChangePasswordPage = () => (
   <>
     <ChangePasswordContainer />
