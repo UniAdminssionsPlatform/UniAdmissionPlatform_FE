a9fad07a530ebbde6b39d66333381ed58a34946b
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-07-03 15:18:09 +0700

    [UAP-187] Implement Manage Profile (#79)
    * [UAP-163] [FE] Implement manage Profile
    
    * [UAP-178] : [FE] Implement manage certificate (role Student) ( View and Update)
    
    * [UAP-202] Improve score student
    
    Co-authored-by: Nham Duc Dat <datndse141132@fpt.edu.vn>

---
+++ src/pages/HighschoolProfilePage/HighschoolProfilePage.jsx
@@ -0,0 +1,10 @@
+import HighSchoolProfileContainer from '../../features/public/highSchoolProfile/HighSchoolProfile.container';
+import React from 'react';
+
+const HighSchoolProfilePage = () => (
+  <>
+    <HighSchoolProfileContainer />
+  </>
+);
+
+export default HighSchoolProfilePage;
---
+++ src/pages/HighschoolProfilePage/UpdateHighschoolProfilePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UpdateHighSchoolProfileContainer from '../../features/public/highschoolProfile/EditHighschoolProfile.container';
+
+const UpdateHighSchoolPage = () => (
+  <>
+    <UpdateHighSchoolProfileContainer />
+  </>
+);
+
+export default UpdateHighSchoolPage;
