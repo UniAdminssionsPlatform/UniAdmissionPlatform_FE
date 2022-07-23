ceef161f4ce351777f990a28e77c4b5960e8a6d4
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-07-24 02:05:08 +0700

    [UAP-285] Comment feature (#100)

--- src/components/commons/SingleCommentForm/SingleCommentForm.component.jsx
+++ src/components/commons/SingleCommentForm/SingleCommentForm.component.jsx
@@ -1,36 +1,8 @@
 import { Input } from 'antd';
-import ButtonPrimary from '../../field/ButtonPrimary/ButtonPrimary.component';
-import ButtonSecondary from '../../field/ButtonSecondary/ButtonSecondary.component';
 import React from 'react';
 
 const { TextArea } = Input;
 
-const SingleCommentForm = ({
-  className = 'mt-5',
-  commentId,
-  onClickSubmit,
-  onClickCancel,
-  defaultValue = '',
-  textareaRef,
-  rows = 4
-}) => (
-  <form action='#' className={`nc-SingleCommentForm ${className}`}>
-    <TextArea
-      placeholder='Add to discussion'
-      ref={textareaRef}
-      required={true}
-      defaultValue={defaultValue}
-      rows={rows}
-    />
-    <div className='mt-2 space-x-3'>
-      <ButtonPrimary onClick={() => onClickSubmit(commentId)} type='submit'>
-        Submit
-      </ButtonPrimary>
-      <ButtonSecondary type='button' onClick={() => onClickCancel(commentId)}>
-        Cancel
-      </ButtonSecondary>
-    </div>
-  </form>
-);
+const SingleCommentForm = () => <></>;
 
 export default SingleCommentForm;
--- src/services/PublishService.js
+++ src/services/PublishService.js
@@ -1,3 +1,10 @@
 import { CallAPI } from './axiosBase';
-import { ENDPOINT_GET_EVENT_PUBLISH_BY_ID } from '../constants/Endpoints/PublishEndpoint';
+import {
+  ENDPOINT_COMMENT_EVENT_BY_EVENT_ID,
+  ENDPOINT_GET_COMMENT_BY_EVENT_ID,
+  ENDPOINT_GET_EVENT_PUBLISH_BY_ID
+} from '../constants/Endpoints/PublishEndpoint';
 export const getEventPublishByIdService = (id) => CallAPI(`${ENDPOINT_GET_EVENT_PUBLISH_BY_ID}${id}`, 'GET');
+export const getCommentByEventIdService = (data) =>
+  CallAPI(`${ENDPOINT_GET_COMMENT_BY_EVENT_ID}?event-id=${data.eventId}&page=${data.page}&limit=${data.limit}`);
+export const commentToTheEventService = (data) => CallAPI(`${ENDPOINT_COMMENT_EVENT_BY_EVENT_ID}`, 'POST', data);
