2d5dd04164caab9ce3e7ae7a0caf9ae397a11655
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-07-30 10:57:34 +0700

    [UAP-292] - New design for dashboard

--- src/router/ProSideBar.component.jsx
+++ src/router/ProSideBar.component.jsx
@@ -9,61 +9,63 @@ import NewReleasesRoundedIcon from '@mui/icons-material/NewReleasesRounded';
 const ProSideBarComponent = () => {
   const { Text, Title, Link } = Typography;
   return (
-    <ProSidebar>
-      <SidebarHeader>
-        <div
-          style={{
-            padding: '24px',
-            textTransform: 'uppercase',
-            fontWeight: 'bold',
-            fontSize: 14,
-            letterSpacing: '1px',
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            whiteSpace: 'nowrap'
-          }}>
-          Uni PlatForm
-        </div>
-      </SidebarHeader>
-      <Menu iconShape='circle'>
-        <MenuItem icon={<HomeRoundedIcon />}>
-          <Text strong>Trang chủ</Text>
-        </MenuItem>
-        <MenuItem icon={<StarBorderRoundedIcon />}>
-          <Text strong>Sự kiện nổi bật</Text>
-        </MenuItem>
-        <MenuItem icon={<VerifiedRoundedIcon />}>
-          <Text strong>Sự kiện trong ngày</Text>
-        </MenuItem>
-        <MenuItem icon={<FlashOnRoundedIcon />}>
-          <Text strong>Sự kiện sắp diễn ra</Text>
-        </MenuItem>
-        <MenuItem icon={<FlashOnRoundedIcon />}>
-          <Text strong>Tin tức mới nhất</Text>
-        </MenuItem>
-        <MenuItem icon={<NewReleasesRoundedIcon />}>
-          <Text strong>Tin tức theo dõi</Text>
-        </MenuItem>
-      </Menu>
-      <SidebarFooter>
-        <div
-          style={{
-            padding: '24px',
-            textTransform: 'uppercase',
-            fontWeight: 'bold',
-            fontSize: 14,
-            letterSpacing: '1px',
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            whiteSpace: 'nowrap'
-          }}>
-          <Space direction={'vertical'}>
-            <Text>Thông tin thêm</Text>
-            <Link>Liên lạc với chúng tôi</Link>
-          </Space>
-        </div>
-      </SidebarFooter>
-    </ProSidebar>
+    <div className='z-11'>
+      <ProSidebar>
+        <SidebarHeader>
+          <div
+            style={{
+              padding: '24px',
+              textTransform: 'uppercase',
+              fontWeight: 'bold',
+              fontSize: 14,
+              letterSpacing: '1px',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap'
+            }}>
+            Uni PlatForm
+          </div>
+        </SidebarHeader>
+        <Menu iconShape='circle'>
+          <MenuItem icon={<HomeRoundedIcon />}>
+            <Text strong>Trang chủ</Text>
+          </MenuItem>
+          <MenuItem icon={<StarBorderRoundedIcon />}>
+            <Text strong>Sự kiện nổi bật</Text>
+          </MenuItem>
+          <MenuItem icon={<VerifiedRoundedIcon />}>
+            <Text strong>Sự kiện trong ngày</Text>
+          </MenuItem>
+          <MenuItem icon={<FlashOnRoundedIcon />}>
+            <Text strong>Sự kiện sắp diễn ra</Text>
+          </MenuItem>
+          <MenuItem icon={<FlashOnRoundedIcon />}>
+            <Text strong>Tin tức mới nhất</Text>
+          </MenuItem>
+          <MenuItem icon={<NewReleasesRoundedIcon />}>
+            <Text strong>Tin tức theo dõi</Text>
+          </MenuItem>
+        </Menu>
+        <SidebarFooter>
+          <div
+            style={{
+              padding: '24px',
+              textTransform: 'uppercase',
+              fontWeight: 'bold',
+              fontSize: 14,
+              letterSpacing: '1px',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap'
+            }}>
+            <Space direction={'vertical'}>
+              <Text>Thông tin thêm</Text>
+              <Link>Liên lạc với chúng tôi</Link>
+            </Space>
+          </div>
+        </SidebarFooter>
+      </ProSidebar>
+    </div>
   );
 };
 export default ProSideBarComponent;
