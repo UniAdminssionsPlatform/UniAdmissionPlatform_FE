b67799b031b7947749cee3f3189886f9fe0f5ec6
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-08-08 00:35:54 +0700

    [UAP-343] - Implement follow university (#128)

--- src/features/public/homePage/components/ButtonTime.component.jsx
+++ src/features/public/homePage/components/ButtonTime.component.jsx
@@ -26,7 +26,7 @@ const ButtonTimeComponent = (props) => {
           <div
             className={`nc-PostCardCommentBtn relative items-center min-w-[68px] rounded-full text-neutral-6000 bg-orange-300 transition-colors dark:text-teal-600 dark:bg-neutral-800 hover:bg-teal-50 dark:hover:bg-teal-100 hover:text-teal-600 dark:hover:text-teal-500 flex px-3 h-8 text-xs`}>
             <span className='ml-1 text-neutral-900 dark:text-neutral-200'>
-              <CalendarTodayIcon style={{ fontSize: 'medium' }} /> <Text type='secondary'>Đã theo dõi</Text>
+              <Text type='secondary'>Đã theo dõi</Text>
             </span>
           </div>
         ) : null}
