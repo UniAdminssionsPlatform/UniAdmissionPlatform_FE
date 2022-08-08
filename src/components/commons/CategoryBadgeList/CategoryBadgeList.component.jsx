c14899e4d666b2b60cddc775ba1aeb4db4c98760
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-08-09 03:43:55 +0700

    [UAP-306] - List news dashboard

--- src/components/commons/CategoryBadgeList/CategoryBadgeList.component.jsx
+++ src/components/commons/CategoryBadgeList/CategoryBadgeList.component.jsx
@@ -1,15 +1,15 @@
 import Badge from '../Badge/Badge.component';
 import React from 'react';
+import { COLOR_ICON, COLOR_SLOT_OPEN } from '../../../constants/Color';
 
 const CategoryBadgeList = (props) => {
-  const { event } = props;
+  const { tagList } = props;
   return (
-    <></>
-    // <div className={`nc-CategoryBadgeList ${className}`} data-nc-id='CategoryBadgeList'>
-    //   {event.map((item, index) => (
-    //     <Badge className={itemClass} key={index} name={item.name} href={item.href} color={item.color} />
-    //   ))}
-    // </div>
+    <div className={`nc-CategoryBadgeList`} data-nc-id='CategoryBadgeList'>
+      {tagList.map((item, index) => (
+        <Badge key={index} name={item.name} href={item.id} color={'blue'} />
+      ))}
+    </div>
   );
 };
 
