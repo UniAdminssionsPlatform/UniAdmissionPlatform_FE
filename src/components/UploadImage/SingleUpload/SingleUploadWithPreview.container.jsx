463e0a04b7f5fc86cf39a02adca2b625f65cd9ce
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-08-09 15:32:14 +0700

    [UAP-350] Upload production (#137)
    * [UAP-350] Upload production
    
    * [UAP-350] Upload production
    
    * [UAP-350] Upload production
    
    * [UAP-350] Upload production

--- src/components/UploadImage/SingleUpload/SingleUploadWithPreview.container.jsx
+++ src/components/UploadImage/SingleUpload/SingleUploadWithPreview.container.jsx
@@ -1,39 +1,27 @@
 import { Button, Upload, message } from 'antd';
-import { HOST_UPLOAD, UPLOAD_A_NEW_IMAGE_ENDPOINT } from '../../../constants/Endpoints/FilesEndpoint';
-import { TOKEN_KEY } from '../../../constants/AppConst';
 import { UploadOutlined } from '@ant-design/icons';
-import Cookies from 'js-cookie';
+import { uploadImageService } from '../../../services/AdminUniversityService/UploadImageService';
 import React from 'react';
 
 const SingleUploadWithPreviewContainer = (props) => {
   const { setImageUrl } = props;
-  const DragProp = {
-    name: 'file',
-    action: `${HOST_UPLOAD}${UPLOAD_A_NEW_IMAGE_ENDPOINT}`,
-    onChange(info) {
-      if (info.file.status !== 'uploading') console.log(info.file, info.fileList);
-      if (info.file.status === 'done') {
-        message.success(`${info.file.name} Tải hình ảnh lên thành công`);
-        setImageUrl(info.file.response.data.fileUrl);
-      } else if (info.file.status === 'error') message.error(`${info.file.name} Tỉa hình ảnh thất bại`);
-    },
-    progress: {
-      strokeColor: {
-        '0%': '#108ee9',
-        '100%': '#87d068'
-      },
-      strokeWidth: 3,
-      format: (percent) => percent && `${parseFloat(percent.toFixed(2))}%`
-    }
+  const uploadImage = async (options) => {
+    const { onSuccess, onError, file } = options;
+    const fmData = new FormData();
+    fmData.append('file', file);
+    uploadImageService(fmData)
+      .then((res) => {
+        onSuccess('Ok');
+        message.success(`Tải hình ảnh lên thành công`);
+        setImageUrl(res.data.data.fileUrl);
+      })
+      .catch((err) => {
+        message.error(`Tải hình ảnh thất bại`);
+        onError({ err });
+      });
   };
-  let token = null;
-  token = Cookies.get(TOKEN_KEY);
   return (
-    <Upload
-      {...DragProp}
-      headers={{ 'x-token': token && token !== 'undefined' ? token : null }}
-      listType='picture'
-      multiple={false}>
+    <Upload customRequest={uploadImage} listType='picture' multiple={false}>
       <Button icon={<UploadOutlined />}>Bấm vào đây để tải lên</Button>
     </Upload>
   );
