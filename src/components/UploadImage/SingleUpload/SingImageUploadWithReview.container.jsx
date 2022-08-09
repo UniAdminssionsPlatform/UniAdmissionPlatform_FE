347ef5492e9a6764e735544bb331e5a7003619e2
BanhsBao <huynhbaofaker@gmail.com>
2022-08-09 15:30:16 +0700

    [UAP-350] Upload production

--- src/components/UploadImage/SingleUpload/SingImageUploadWithReview.container.jsx
+++ src/components/UploadImage/SingleUpload/SingImageUploadWithReview.container.jsx
@@ -1,9 +1,7 @@
 import 'antd/dist/antd.css';
-import { HOST_UPLOAD, UPLOAD_A_NEW_IMAGE_ENDPOINT } from '../../../constants/Endpoints/FilesEndpoint';
 import { Modal, Upload, message } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
-import { TOKEN_KEY } from '../../../constants/AppConst';
-import Cookies from 'js-cookie';
+import { uploadImageService } from '../../../services/AdminUniversityService/UploadImageService';
 import React, { useState } from 'react';
 
 const SingleImageUploadWithReviewContainer = (props) => {
@@ -16,39 +14,31 @@ const SingleImageUploadWithReviewContainer = (props) => {
     new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.readAsDataURL(file);
-
       reader.onload = () => resolve(reader.result);
-
       reader.onerror = (error) => reject(error);
     });
-  const requestProps = {
-    name: 'file',
-    action: `${HOST_UPLOAD}${UPLOAD_A_NEW_IMAGE_ENDPOINT}`,
-    onChange(info) {
-      if (info.file.status !== 'uploading') console.log(info.file, info.fileList);
-      if (info.file.status === 'done') {
-        message.success(`${info.file.name} Tải hình ảnh lên thành công`);
-        setImageUrl(info.file.response.data.fileUrl);
-      } else if (info.file.status === 'error') message.error(`${info.file.name} Tải hình ảnh thất bại`);
-    },
-    progress: {
-      strokeColor: {
-        '0%': '#108ee9',
-        '100%': '#87d068'
-      },
-      strokeWidth: 3,
-      format: (percent) => percent && `${parseFloat(percent.toFixed(2))}%`
-    }
+  const uploadImage = async (options) => {
+    const { onSuccess, onError, file } = options;
+    const fmData = new FormData();
+    fmData.append('file', file);
+    uploadImageService(fmData)
+      .then((res) => {
+        onSuccess('Ok');
+        message.success(`Tải hình ảnh lên thành công`);
+        setImageUrl(res.data.data.fileUrl);
+      })
+      .catch((err) => {
+        message.error(`Tải hình ảnh thất bại`);
+        onError({ err });
+      });
   };
+
   const handlePreview = async (file) => {
     if (!file.url && !file.preview) file.preview = await getBase64(file.originFileObj);
-
     setPreviewImage(file.url || file.preview);
     setPreviewVisible(true);
     setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
   };
-  let token = null;
-  token = Cookies.get(TOKEN_KEY);
   const handleCancel = () => setPreviewVisible(false);
   const uploadButton = (
     <div>
@@ -72,8 +62,7 @@ const SingleImageUploadWithReviewContainer = (props) => {
   return (
     <>
       <Upload
-        {...requestProps}
-        headers={{ 'x-token': token && token !== 'undefined' ? token : null, 'content-type': 'multipart/form-data' }}
+        customRequest={uploadImage}
         onPreview={handlePreview}
         onChange={handleChange}
         listType='picture-card'
