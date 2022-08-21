b8db0e3e7b7353849e7e6fe8751c83947dc7a791
uikui <tinnt@gmail.com>
2022-08-21 23:01:44 +0700

    hot fix

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -4,7 +4,7 @@ import { DETAIL_EVENT_ENDPOINT, GET_LIST_EVENT_ENDPOINT } from '../constants/End
 export const getDetailEventByEventIdService = (data) => CallAPI(`${DETAIL_EVENT_ENDPOINT}/${data}`, 'get');
 export const getListEventService = (data) =>
   CallAPI(
-    `${GET_LIST_EVENT_ENDPOINT}?page=${data.page}&limit=${data.limit}&status=${data.status}&sort=CreateAt%20desc`,
+    `${GET_LIST_EVENT_ENDPOINT}?page=${data.page}&limit=${data.limit}&status=${data.status}&sort=CreatedAt%20desc`,
     'get',
     '',
     data
