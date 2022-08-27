c6d63496e543fc78bba832fbf6ecdcdd96e2ccea
uikui <tinnt@gmail.com>
2022-08-27 14:18:22 +0700

    [HOT-FIX] Confirm registering event detail

--- src/features/universityManager/bookingFlow/confirmBooking/ConfirmBooking.component.jsx
+++ src/features/universityManager/bookingFlow/confirmBooking/ConfirmBooking.component.jsx
@@ -1,29 +1,48 @@
-import { Button, Descriptions, Space } from 'antd';
+import { Button, Descriptions, Space, Typography, Collapse } from 'antd';
+import moment from 'moment';
 import React from 'react';
 import MarkdownViewComponent from '../../../../components/MarkdownView/MarkdownView.component';
 
 const ConfirmBookingComponent = (props) => {
   const { currenSlotSelected, currentEventSelected, handleSubmit } = props;
+  const { Panel } = Collapse;
+  const { Text, Title } = Typography;
+  const onChange = () => {};
   return (
-    <>
-      <Descriptions title='Thông tin sự kiện' bordered layout='vertical'>
-        <Descriptions.Item label='Tên sự kiện'>{currentEventSelected?.name}</Descriptions.Item>
-        <Descriptions.Item label='Host chương trình'>{currentEventSelected?.hostName}</Descriptions.Item>
-        <Descriptions.Item label='Chú giải'>
-          <MarkdownViewComponent str={currentEventSelected?.description} />
-        </Descriptions.Item>
-        <Descriptions.Item label='Chú thích'>{currentEventSelected?.shortDescription}</Descriptions.Item>
-      </Descriptions>
-      <Descriptions title='Thông tin Slot' bordered layout='vertical'>
-        <Descriptions.Item label='Thời gian bắt đầu'>{currenSlotSelected?.startDate}</Descriptions.Item>
-        <Descriptions.Item label='Thời gian kết thúc'>{currenSlotSelected?.endDate}</Descriptions.Item>
-        <Descriptions.Item label='Đăng ký sự kiện'>
-          <Button type={'primary'} onClick={handleSubmit} danger shape={'round'}>
-            Đăng ký sự kiện
-          </Button>
-        </Descriptions.Item>
-      </Descriptions>
-    </>
+    <Space direction='vertical'>
+      <br />
+      <div>
+        <Title level='3'>Thông tin đăng ký sự kiện</Title>
+        <Text strong> {currentEventSelected?.shortDescription}</Text>
+      </div>
+      <div>
+        <Descriptions title='Thông tin sự kiện' bordered layout='vertical'>
+          <Descriptions.Item label={<Text strong>Tên sự kiện</Text>}>{currentEventSelected?.name}</Descriptions.Item>
+          <Descriptions.Item label={<Text strong>Host chương trình</Text>}>
+            {currentEventSelected?.hostName}
+          </Descriptions.Item>
+        </Descriptions>
+        <Descriptions title={<Text strong>Thông tin slot đăng ký</Text>} bordered layout='vertical'>
+          <Descriptions.Item label={<Text strong>Thời gian bắt đầu</Text>}>
+            <Text strong>{moment(currenSlotSelected?.startDate).format('LLL')}</Text>
+          </Descriptions.Item>
+          <Descriptions.Item label={<Text strong>Thời gian kết thúc</Text>}>
+            <Text strong>{moment(currenSlotSelected?.endDate).format('LLL')}</Text>
+          </Descriptions.Item>
+          <Descriptions.Item label={<Text strong>Đăng ký sự kiện</Text>}>
+            <Button type={'primary'} onClick={handleSubmit} danger shape={'round'}>
+              Đăng ký sự kiện
+            </Button>
+          </Descriptions.Item>
+        </Descriptions>
+        <br />
+        <Collapse>
+          <Panel header={<Text strong>Nội dung sự kiện</Text>} key='1'>
+            <MarkdownViewComponent str={currentEventSelected?.description} />
+          </Panel>
+        </Collapse>
+      </div>
+    </Space>
   );
 };
 export default ConfirmBookingComponent;
